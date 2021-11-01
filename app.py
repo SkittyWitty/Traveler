1af28370f9231241b1dea3fee9abf61c44407688
Mindy DeWaal <kawiititan@gmail.com>
2021-10-31 19:32:51 -0700

    Basic colors stored in app object. Adding a title/caption to screen display

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ from pygame.locals import *
 class App:
     # common colors
     _white  = (255, 255, 255)
-    _grass  = ()
-    _ground = ()
-    _street = ()
-    _water  = ()
+    _grass  = ( 55, 177,  33)
+    _ground = (177, 107,  32)
+    _street = (120, 120, 120)
+    _water  = ( 32,  54, 177)
 
     def __init__(self):
         self._running = True
         self._displaySurf = None
-        self._screen_display = pygame.display
+        self._screenSurf = pygame.display
         self.size = self.width, self.height = 640, 480 # Size is a tuple containing width and height
 
     def onInit(self):
         pygame.init()
         self._displaySurf = pygame.display.set_mode(self.size, pygame.HWSURFACE)
+        self._screenSurf.set_caption("The Traveler")
         self._running = True
         self._imageSurf = pygame.image.load("./resources/characters/traveler.png").convert()
 
@@ -29,6 +30,7 @@ class App:
         pass
     def onRender(self):
         self._displaySurf.blit(self._imageSurf, (0,0))
+        self._screenSurf.update()
         pygame.display.flip()
 
     def onCleanup(self):
