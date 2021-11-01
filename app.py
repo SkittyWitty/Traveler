2e678c63d452e19e4500d6435d4a76dfcb8166cd
Mindy DeWaal <kawiititan@gmail.com>
2021-10-31 19:22:44 -0700

    Rendering 1 image

--- app.py
+++ app.py
@@ -2,15 +2,24 @@ import pygame
 from pygame.locals import *
 
 class App:
+    # common colors
+    _white  = (255, 255, 255)
+    _grass  = ()
+    _ground = ()
+    _street = ()
+    _water  = ()
+
     def __init__(self):
         self._running = True
         self._displaySurf = None
+        self._screen_display = pygame.display
         self.size = self.width, self.height = 640, 480 # Size is a tuple containing width and height
 
     def onInit(self):
         pygame.init()
         self._displaySurf = pygame.display.set_mode(self.size, pygame.HWSURFACE)
         self._running = True
+        self._imageSurf = pygame.image.load("./resources/characters/traveler.png").convert()
 
     def onEvent(self, event):
         if event.type == pygame.QUIT:
@@ -19,7 +28,8 @@ class App:
     def onLoop(self):
         pass
     def onRender(self):
-        pass
+        self._displaySurf.blit(self._imageSurf, (0,0))
+        pygame.display.flip()
 
     def onCleanup(self):
         pygame.quit()
